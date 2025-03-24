55a952da9970233d4258c8bce02cd54bdbaea208
J <6235953@bdeb.qc.ca>
2025-03-24 09:42:29 -0400

    Update

--- api/index.js
+++ api/index.js
@@ -6,29 +6,10 @@ const app = express();
 const port = 8000;
 const cors = require('cors');
 
-
 // middleware
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 mongoose.connect("mongodb+srv://nom_utilisateur:mot_de_passe@ridew.xta2m.mongodb.net/", {
     useNewUrlParser: true,
